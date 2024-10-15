35520910ae8c731a9c889b0f2e4f3745659981dc
Amdadullah-Raihan <amdadullahrayhan@gmail.com>
2024-10-15 16:10:04 +0600

    feat: add sensory impact to the result

--- frontend/src/pages/Result.jsx
+++ frontend/src/pages/Result.jsx
@@ -2,143 +2,173 @@ import React from "react";
 
 const Result = ({ score, handleReset }) => {
   return (
-    <div className="space-y-3 grid grid-cols-1 max-w-screen-lg mx-auto">
-      <h2 className="text-2xl font-bold text-center mb-6">Your Scores</h2>
+    <div className="">
+      <div className="space-y-3 grid grid-cols-1 max-w-screen-lg mx-auto">
+        <h2 className="text-2xl font-bold text-center mb-6">Your Scores</h2>
 
-      {/* Row 1 */}
-      <div className="flex relative w-full">
-        <p className="-rotate-90 absolute top-1/2  right-[97%] font-bold text-green-500">
-          EXPRESSED
-        </p>
-        <div className="grid grid-cols-4 gap-3 w-full">
-          <div>
-            <p className="uppercase text-center text-blue-500 font-bold">
-              Social Inclusion
-            </p>
-            <div className="border rounded-lg overflow-hidden">
-              <p className="bg-gray-200 p-3 text-center">
-                Expressed Social Inclusion
+        {/* Row 1 */}
+        <div className="flex relative w-full">
+          <p className="-rotate-90 absolute top-1/2  right-[97%] font-bold text-green-500">
+            EXPRESSED
+          </p>
+          <div className="grid grid-cols-4 gap-3 w-full">
+            <div>
+              <p className="uppercase text-center text-blue-500 font-bold">
+                Social Inclusion
               </p>
-              <p className="p-6 text-center text-xl font-medium">{score.EI}</p>
+              <div className="border rounded-lg overflow-hidden">
+                <p className="bg-gray-200 p-3 text-center">
+                  Expressed Social Inclusion
+                </p>
+                <p className="p-6 text-center text-xl font-medium">
+                  {score.EI}
+                </p>
+              </div>
+            </div>
+            <div>
+              <p className="uppercase text-center text-cyan-500 font-bold">
+                Decision Making
+              </p>
+              <div className="border rounded-lg overflow-hidden">
+                <p className="bg-gray-200 p-3 text-center">
+                  Expressed Decision Making
+                </p>
+                <p className="p-6 text-center text-xl font-medium">
+                  {score.EC}
+                </p>
+              </div>
+            </div>
+            <div>
+              <p className="uppercase text-center text-slate-500 font-bold">
+                Acceptance
+              </p>
+              <div className="border rounded-lg overflow-hidden">
+                <p className="bg-gray-200 p-3 text-center">
+                  Expressed Acceptance
+                </p>
+                <p className="p-6 text-center text-xl font-medium">
+                  {score.EA}
+                </p>
+              </div>
+            </div>
+            <div>
+              <br />
+              <div className="border rounded-lg overflow-hidden">
+                <p className="bg-green-500 text-white p-3 text-center">
+                  Total Expressed Behavior
+                </p>
+                <p className="p-6 text-center text-xl font-medium">
+                  {score.EI + score.EC + score.EA}
+                </p>
+              </div>
             </div>
           </div>
-          <div>
-            <p className="uppercase text-center text-cyan-500 font-bold">
-              Decision Making
-            </p>
+        </div>
+
+        {/* Row 2 */}
+
+        <div className="flex relative">
+          <p className="-rotate-90 absolute top-1/2  right-[98.5%] text-purple-500 font-bold">
+            WANTED
+          </p>
+          <div className="grid grid-cols-4 gap-3 w-full">
             <div className="border rounded-lg overflow-hidden">
               <p className="bg-gray-200 p-3 text-center">
-                Expressed Decision Making
+                Wanted Social Inclusion
               </p>
-              <p className="p-6 text-center text-xl font-medium">{score.EC}</p>
+              <p className="p-6 text-center text-xl font-medium">{score.WI}</p>
             </div>
-          </div>
-          <div>
-            <p className="uppercase text-center text-slate-500 font-bold">
-              Acceptance
-            </p>
+
             <div className="border rounded-lg overflow-hidden">
               <p className="bg-gray-200 p-3 text-center">
-                Expressed Acceptance
+                Wanted Decision Making
               </p>
-              <p className="p-6 text-center text-xl font-medium">{score.EA}</p>
+              <p className="p-6 text-center text-xl font-medium">{score.WC}</p>
             </div>
-          </div>
-          <div>
-            <br />
+
             <div className="border rounded-lg overflow-hidden">
-              <p className="bg-green-500 text-white p-3 text-center">
-                Total Expressed Behavior
+              <p className="bg-gray-200 p-3 text-center">Wanted Acceptance</p>
+              <p className="p-6 text-center text-xl font-medium">{score.WA}</p>
+            </div>
+
+            <div className="border rounded-lg overflow-hidden">
+              <p className="bg-purple-500 text-white p-3 text-center">
+                Total Wanted Behavior
               </p>
               <p className="p-6 text-center text-xl font-medium">
-                {score.EI + score.EC + score.EA}
+                {score.WI + score.WC + score.WA}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Row 3 */}
+        <div className="flex relative">
+          <div className="grid grid-cols-4 gap-3 w-full">
+            <div className="border rounded-lg overflow-hidden">
+              <p className="bg-blue-500 text-white p-3 text-center">
+                Total For Social Inclusion
+              </p>
+              <p className="p-6 text-center text-xl font-medium">
+                {score.WI + score.EI}
+              </p>
+            </div>
+
+            <div className="border rounded-lg overflow-hidden">
+              <p className="bg-cyan-500 text-white p-3 text-center">
+                Total For Decision Making
+              </p>
+              <p className="p-6 text-center text-xl font-medium">
+                {score.WC + score.EC}
+              </p>
+            </div>
+
+            <div className="border rounded-lg overflow-hidden">
+              <p className="bg-slate-500 p-3 text-center text-white">
+                Total For Acceptance
+              </p>
+              <p className="p-6 text-center text-xl font-medium">
+                {score.WA + score.EA}
+              </p>
+            </div>
+
+            <div className="border rounded-lg overflow-hidden">
+              <p className="bg-orange-500 text-white p-3 text-center">
+                Overall
+              </p>
+              <p className="p-6 text-center text-xl font-medium">
+                {score.WI +
+                  score.WC +
+                  score.WA +
+                  score.EI +
+                  score.EC +
+                  score.EA}
               </p>
             </div>
           </div>
         </div>
       </div>
-
-      {/* Row 2 */}
-
-      <div className="flex relative">
-        <p className="-rotate-90 absolute top-1/2  right-[98.5%] text-purple-500 font-bold">
-          WANTED
-        </p>
-        <div className="grid grid-cols-4 gap-3 w-full">
+      <div className="flex justify-end my-10 max-w-screen-lg  mx-auto">
+        <div className="w-60">
+          <p className="uppercase text-center text-orange-500 font-bold">
+            Sensory Impact
+          </p>
           <div className="border rounded-lg overflow-hidden">
-            <p className="bg-gray-200 p-3 text-center">
-              Wanted Social Inclusion
+            <p className="bg-orange-500 text-white p-3 text-center">
+              Sensory Impact
             </p>
-            <p className="p-6 text-center text-xl font-medium">{score.WI}</p>
-          </div>
-
-          <div className="border rounded-lg overflow-hidden">
-            <p className="bg-gray-200 p-3 text-center">
-              Wanted Decision Making
-            </p>
-            <p className="p-6 text-center text-xl font-medium">{score.WC}</p>
-          </div>
-
-          <div className="border rounded-lg overflow-hidden">
-            <p className="bg-gray-200 p-3 text-center">Wanted Acceptance</p>
-            <p className="p-6 text-center text-xl font-medium">{score.WA}</p>
-          </div>
-
-          <div className="border rounded-lg overflow-hidden">
-            <p className="bg-purple-500 text-white p-3 text-center">
-              Total Wanted Behavior
-            </p>
-            <p className="p-6 text-center text-xl font-medium">
-              {score.WI + score.WC + score.WA}
-            </p>
+            <p className="p-6 text-center text-xl font-medium">{score.SI}</p>
           </div>
         </div>
       </div>
-
-      {/* Row 3 */}
-      <div className="flex relative">
-        <div className="grid grid-cols-4 gap-3 w-full">
-          <div className="border rounded-lg overflow-hidden">
-            <p className="bg-blue-500 text-white p-3 text-center">
-              Total For Social Inclusion
-            </p>
-            <p className="p-6 text-center text-xl font-medium">
-              {score.WI + score.EI}
-            </p>
-          </div>
-
-          <div className="border rounded-lg overflow-hidden">
-            <p className="bg-cyan-500 text-white p-3 text-center">
-              Total For Decision Making
-            </p>
-            <p className="p-6 text-center text-xl font-medium">
-              {score.WC + score.EC}
-            </p>
-          </div>
-
-          <div className="border rounded-lg overflow-hidden">
-            <p className="bg-slate-500 p-3 text-center text-white">
-              Total For Acceptance
-            </p>
-            <p className="p-6 text-center text-xl font-medium">
-              {score.WA + score.EA}
-            </p>
-          </div>
-
-          <div className="border rounded-lg overflow-hidden">
-            <p className="bg-orange-500 text-white p-3 text-center">Overall</p>
-            <p className="p-6 text-center text-xl font-medium">
-              {score.WI + score.WC + score.WA + score.EI + score.EC + score.EA}
-            </p>
-          </div>
-        </div>
+      <div className="flex justify-center my-4">
+        <button
+          onClick={handleReset}
+          className="bg-indigo-500 text-white px-4 py-2 rounded-md mx-auto"
+        >
+          Try Again
+        </button>
       </div>
-      <button
-        onClick={handleReset}
-        className="bg-indigo-500 text-white px-4 py-2 rounded-md"
-      >
-        Try Again
-      </button>
     </div>
   );
 };
