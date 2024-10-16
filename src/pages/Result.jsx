d8571df17fa86f461b5a1fe50e0123c6dd5fbca9
Amdadullah-Raihan <amdadullahrayhan@gmail.com>
2024-10-16 16:55:15 +0600

    style: make the result page mobile responsive

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -8,12 +8,12 @@ const Result = ({ score, handleReset }) => {
   }, []);
 
   return (
-    <div className="">
-      <div className="space-y-3 grid grid-cols-1 w-[1024px] mx-auto overflow-x-auto px-5">
-        <h2 className="text-2xl font-bold lg:text-center mb-6">Your Scores</h2>
+    <div className="overflow-x-auto flex flex-col justify-center">
+      <div className="space-y-3 grid grid-cols-1  mx-auto  overflow-x-auto px-5 pb-2 ">
+        <h2 className="text-2xl font-bold text-center mb-6">Your Scores</h2>
 
         {/* Row 1 */}
-        <div className="flex relative w-full ml-6 lg:ml-2">
+        <div className="flex  relative w-[1024px] ml-3  ">
           <p className="-rotate-90 absolute top-1/2  right-[97%] font-bold text-green-500">
             EXPRESSED
           </p>
@@ -73,7 +73,7 @@ const Result = ({ score, handleReset }) => {
 
         {/* Row 2 */}
 
-        <div className="flex relative  ml-6 lg:ml-2">
+        <div className="flex relative  w-[1024px] ml-3">
           <p className="-rotate-90 absolute top-1/2  right-[98.5%] text-purple-500 font-bold">
             WANTED
           </p>
@@ -109,7 +109,7 @@ const Result = ({ score, handleReset }) => {
         </div>
 
         {/* Row 3 */}
-        <div className="flex relative  ml-6 lg:ml-2">
+        <div className="flex relative  w-[1024px] ml-3">
           <div className="grid grid-cols-4 gap-3 w-full">
             <div className="border rounded-lg overflow-hidden">
               <p className="bg-blue-500 text-white p-3 text-center">
@@ -154,8 +154,10 @@ const Result = ({ score, handleReset }) => {
           </div>
         </div>
       </div>
-      <div className="w-full flex justify-between my-10 max-w-screen-lg  mx-auto">
-        <div className="w-60">
+
+      {/* Sensory Impact */}
+      <div className="max-w-screen-lg mx-auto mt-10 space-y-5 ">
+        <div className="w-full md:w-60 pl-5">
           <p className="uppercase text-center text-orange-500 font-bold">
             Sensory Impact
           </p>
@@ -166,18 +168,10 @@ const Result = ({ score, handleReset }) => {
             <p className="p-6 text-center text-xl font-medium">{score.SI}</p>
           </div>
         </div>
-        {nodeEnv === "development" && (
-          <button
-            onClick={handleReset}
-            className="bg-indigo-500 text-white px-4 py-2 rounded-md  h-12"
-          >
-            Try Again
-          </button>
-        )}
       </div>
 
       {/* Graph Section */}
-      <div className="py-16 max-w-screen-lg mx-auto px-5 lg:px-0 ">
+      <div className="pb-16 max-w-screen-lg mx-auto px-5 lg:px-0 ">
         <div className="py-16 max-w-screen-lg mx-auto space-y-16">
           <div className="grid grid-cols-1 gap-4">
             {/* Social Inclusion section */}
